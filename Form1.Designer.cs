26a96b180966d73e12acab4b802e65a7c6678c18
NauberGdS <149209805+NauberGdS@users.noreply.github.com>
2023-11-08 06:36:32 -0300

    Calculadora UAU
    divisão por zero feita, porem contínua o erro de clicar na aba e quebrar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btn7 = new System.Windows.Forms.Button();
             this.btn4 = new System.Windows.Forms.Button();
             this.btn1 = new System.Windows.Forms.Button();
@@ -44,7 +45,7 @@
             this.btn0 = new System.Windows.Forms.Button();
             this.btnApagar = new System.Windows.Forms.Button();
             this.btnIgual = new System.Windows.Forms.Button();
-            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.txbTela = new System.Windows.Forms.TextBox();
             this.txbAux = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -60,6 +61,7 @@
             this.btn7.TabIndex = 0;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn4
             // 
@@ -73,6 +75,7 @@
             this.btn4.TabIndex = 1;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn1
             // 
@@ -86,6 +89,7 @@
             this.btn1.TabIndex = 2;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn8
             // 
@@ -99,6 +103,7 @@
             this.btn8.TabIndex = 3;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn5
             // 
@@ -112,6 +117,7 @@
             this.btn5.TabIndex = 4;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn2
             // 
@@ -125,6 +131,7 @@
             this.btn2.TabIndex = 5;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn9
             // 
@@ -138,6 +145,7 @@
             this.btn9.TabIndex = 6;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn6
             // 
@@ -151,6 +159,7 @@
             this.btn6.TabIndex = 7;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btn3
             // 
@@ -164,6 +173,7 @@
             this.btn3.TabIndex = 8;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btnSoma
             // 
@@ -177,6 +187,7 @@
             this.btnSoma.TabIndex = 9;
             this.btnSoma.Text = "+";
             this.btnSoma.UseVisualStyleBackColor = false;
+            this.btnSoma.Click += new System.EventHandler(this.Operador_Click);
             // 
             // btnSubtracao
             // 
@@ -190,6 +201,7 @@
             this.btnSubtracao.TabIndex = 14;
             this.btnSubtracao.Text = "-";
             this.btnSubtracao.UseVisualStyleBackColor = false;
+            this.btnSubtracao.Click += new System.EventHandler(this.Operador_Click);
             // 
             // btnMultiplicacao
             // 
@@ -203,6 +215,7 @@
             this.btnMultiplicacao.TabIndex = 15;
             this.btnMultiplicacao.Text = "*";
             this.btnMultiplicacao.UseVisualStyleBackColor = false;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.Operador_Click);
             // 
             // btnDivisao
             // 
@@ -216,6 +229,7 @@
             this.btnDivisao.TabIndex = 16;
             this.btnDivisao.Text = "/";
             this.btnDivisao.UseVisualStyleBackColor = false;
+            this.btnDivisao.Click += new System.EventHandler(this.Operador_Click);
             // 
             // btn0
             // 
@@ -229,6 +243,7 @@
             this.btn0.TabIndex = 17;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.Numero_Click);
             // 
             // btnApagar
             // 
@@ -242,6 +257,7 @@
             this.btnApagar.TabIndex = 18;
             this.btnApagar.Text = "C";
             this.btnApagar.UseVisualStyleBackColor = false;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
             // 
             // btnIgual
             // 
@@ -255,23 +271,26 @@
             this.btnIgual.TabIndex = 19;
             this.btnIgual.Text = "=";
             this.btnIgual.UseVisualStyleBackColor = false;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
             // 
-            // txbResultado
+            // txbTela
             // 
-            this.txbResultado.BackColor = System.Drawing.SystemColors.Window;
-            this.txbResultado.Enabled = false;
-            this.txbResultado.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txbResultado.Location = new System.Drawing.Point(8, 39);
-            this.txbResultado.Multiline = true;
-            this.txbResultado.Name = "txbResultado";
-            this.txbResultado.Size = new System.Drawing.Size(272, 46);
-            this.txbResultado.TabIndex = 20;
+            this.txbTela.BackColor = System.Drawing.SystemColors.Window;
+            this.txbTela.Enabled = false;
+            this.txbTela.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTela.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txbTela.Location = new System.Drawing.Point(8, 39);
+            this.txbTela.Multiline = true;
+            this.txbTela.Name = "txbTela";
+            this.txbTela.Size = new System.Drawing.Size(272, 46);
+            this.txbTela.TabIndex = 20;
             // 
             // txbAux
             // 
             this.txbAux.BackColor = System.Drawing.SystemColors.Window;
+            this.txbAux.Enabled = false;
             this.txbAux.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txbAux.Location = new System.Drawing.Point(12, 12);
+            this.txbAux.Location = new System.Drawing.Point(8, 13);
             this.txbAux.Name = "txbAux";
             this.txbAux.Size = new System.Drawing.Size(93, 20);
             this.txbAux.TabIndex = 21;
@@ -280,9 +299,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(289, 346);
             this.Controls.Add(this.txbAux);
-            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.txbTela);
             this.Controls.Add(this.btnIgual);
             this.Controls.Add(this.btnApagar);
             this.Controls.Add(this.btn0);
@@ -299,8 +320,12 @@
             this.Controls.Add(this.btn1);
             this.Controls.Add(this.btn4);
             this.Controls.Add(this.btn7);
+            this.Enabled = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.Click += new System.EventHandler(this.Numero_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -324,7 +349,7 @@
         private System.Windows.Forms.Button btn0;
         private System.Windows.Forms.Button btnApagar;
         private System.Windows.Forms.Button btnIgual;
-        private System.Windows.Forms.TextBox txbResultado;
+        private System.Windows.Forms.TextBox txbTela;
         private System.Windows.Forms.TextBox txbAux;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -12,13 +12,55 @@ namespace CalculadoraUAU
 {
     public partial class Form1 : Form
     {
+        //declarando variaveis universais:
+        DataTable dt = new DataTable();
+        string operato;
+        int ultimonum;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //selecionando botões de operação
+        private void Operador_Click(object sender, EventArgs e)
+        {
+            var botao = (Button)sender;
+            txbTela.Text += botao.Text;
+            operato = botao.Text.ToString();
+        }
 
+        //selecionando botões numéricos
+        private void Numero_Click(object sender, EventArgs e)
+        {
+            var botao = (Button)sender;
+            txbTela.Text += botao.Text;
+            ultimonum = int.Parse(botao.Text);
+        }
 
-      
+        //botão apagar, quando chamado exclui dados da tela
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            txbTela.Clear();
+            txbAux.Clear();
+        }
+
+        //botão igual 
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            if (ultimonum == 0 && operato == "/")
+            {
+                MessageBox.Show("Impossível dividir por zero");
+                txbTela.Clear();
+                txbAux.Clear();
+            }
+            else
+            {
+                var v = dt.Compute(txbTela.Text, "");
+                txbAux.Text = txbTela.Text;
+                txbTela.Text = v.ToString();
+            }
+
+        }
     }
 }
