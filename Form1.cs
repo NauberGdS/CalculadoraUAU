0feec7a38c5eac2b704f5f12675193399be2d42e
NauberGdS <149209805+NauberGdS@users.noreply.github.com>
2023-11-08 08:03:36 -0300

    Calculadora finished
    Tratativas de erros realizadas:
    - divisão por zero
    - primeiro caractér operador
    - duplicidade de operadores
    - formula completa txbTela

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +17,8 @@ namespace CalculadoraUAU
         DataTable dt = new DataTable();
         string operato;
         int ultimonum;
+        private bool numeroClicado = false;
+        private bool operadorClicado = false;
 
         public Form1()
         {
@@ -25,9 +28,18 @@ namespace CalculadoraUAU
         //selecionando botões de operação
         private void Operador_Click(object sender, EventArgs e)
         {
+            //correção erro caso o primeiro caracter seja um operador;
+            if (numeroClicado && !operadorClicado)
+            { 
             var botao = (Button)sender;
             txbTela.Text += botao.Text;
             operato = botao.Text.ToString();
+            operadorClicado = true; // recebe true para não haver duplicidade
+            }
+            else
+            {
+                MessageBox.Show("Selecione os números!");
+            }
         }
 
         //selecionando botões numéricos
@@ -36,6 +48,8 @@ namespace CalculadoraUAU
             var botao = (Button)sender;
             txbTela.Text += botao.Text;
             ultimonum = int.Parse(botao.Text);
+            numeroClicado = true;
+            operadorClicado = false;
         }
 
         //botão apagar, quando chamado exclui dados da tela
@@ -43,24 +57,27 @@ namespace CalculadoraUAU
         {
             txbTela.Clear();
             txbAux.Clear();
+            numeroClicado = false;
         }
 
         //botão igual 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            if (ultimonum == 0 && operato == "/")
-            {
-                MessageBox.Show("Impossível dividir por zero");
-                txbTela.Clear();
-                txbAux.Clear();
+                if (ultimonum == 0 && operato == "/")
+                {
+                    MessageBox.Show("Impossível dividir por zero!");
+                    //chamar a classse apagar, para que o erro do operador antes do numero não ocorra
+                    btnApagar_Click(sender, e);
             }
-            else
-            {
-                var v = dt.Compute(txbTela.Text, "");
-                txbAux.Text = txbTela.Text;
-                txbTela.Text = v.ToString();
-            }
-
+                else
+                {
+                    var v = dt.Compute(txbTela.Text, "");
+                    txbAux.Text = txbTela.Text;
+                    txbTela.Text = v.ToString();
+                operadorClicado = false; // operadorClicado volta a ser falso para não haver duplicidade nos operadores
+                }
+            
         }
+
     }
 }
